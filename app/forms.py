cbda9fb25ff36db0f4cb6a1958d8bef47cbd494a
hiro <yamaoka.hiroshi.55x@st.kyoto-u.ac.jp>
2025-08-28 12:25:24 +0900

    this forms include flask_wtf, which enable us to make input format easily

---
+++ app/forms.py
@@ -0,0 +1,9 @@
+from flask_wtf import FlaskForm
+from wtforms import StringField, PasswordField, BooleanField, SubmitField
+from wtforms.validators import DataRequired
+
+class LoginForms(FlaskForm):
+    username = StringField('username', validators=[DataRequired()])
+    password = StringField('password', validators=[DataRequired()])
+    remember_me = BooleanField('Remember Me')
+    submit = SubmitField('Sign In')
