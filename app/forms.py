30e17fd8d2fcc7c22fad23214ab672988a16d89d
hiro <yamaoka.hiroshi.55x@st.kyoto-u.ac.jp>
2025-08-28 17:02:08 +0900

    references are added

--- app/forms.py
+++ app/forms.py
@@ -2,7 +2,7 @@ from flask_wtf import FlaskForm
 from wtforms import StringField, PasswordField, BooleanField, SubmitField
 from wtforms.validators import DataRequired
 
-class LoginForms(FlaskForm):
+class LoginForm(FlaskForm):
     username = StringField('username', validators=[DataRequired()])
     password = StringField('password', validators=[DataRequired()])
     remember_me = BooleanField('Remember Me')
--- app/routes.py
+++ app/routes.py
@@ -1,7 +1,13 @@
 from app import app
 from flask import render_template
+from app.forms import LoginForm
 
 @app.route('/')
 @app.route('/index')
 def index():
-    return "Hello, World!"
+    return render_template('index.html', title='Home')
+
+@app.route('/login')
+def login():
+    form = LoginForm()
+    return render_template('login_page.html', title='Login', form=form)
