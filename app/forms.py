76777af558f73ba2a19a5bdcbb7a661cc590f192
hiro-mac <kuanshangang@gmail.com>
2025-08-31 17:15:38 +0900

    post form is added

--- app/forms.py
+++ app/forms.py
@@ -1,9 +1,42 @@
 from flask_wtf import FlaskForm
-from wtforms import StringField, PasswordField, BooleanField, SubmitField
-from wtforms.validators import DataRequired
+from wtforms import StringField, PasswordField, BooleanField, SubmitField, TextAreaField
+from wtforms.validators import DataRequired, ValidationError, Email, EqualTo
+from app.models import User # --- Userモデルをインポート ---
+from flask_wtf.file import FileField, FileRequired, FileAllowed
+
 
 class LoginForm(FlaskForm):
     username = StringField('username', validators=[DataRequired()])
     password = StringField('password', validators=[DataRequired()])
     remember_me = BooleanField('Remember Me')
     submit = SubmitField('Sign In')
+
+class RegistrationForm(FlaskForm):
+    username = StringField('Username', validators=[DataRequired()])
+    email = StringField('Emaili', validators=[DataRequired(), Email()])
+    password = PasswordField('Password', validators=[DataRequired()])
+    password2 = PasswordField(
+        'Repeat Password', validators=[DataRequired(), EqualTo('password')])
+    submit = SubmitField('Register')
+    
+    # check whether the username is already used or not
+    def validate_username(self, username):
+        user = User.query.filter_by(username=username.data).first()
+        if user is not None:
+            raise ValidationError('Please use a different username.')
+        
+    
+    # check whether the email is already used or not
+    def validate_email(self, email):
+        user = User.query.filter_by(email=email.data).first()
+        if user is not None:
+            raise ValidationError('Please use a different email address.')
+        
+class PostForm(FlaskForm):
+    image = FileField('Image', validators=[
+        FileRequired(),
+        FileAllowed(['jpg','jpeg','png','gif', 'Images only"'])
+    ])
+    shop_name = StringField('Shop Name', validators=[DataRequired()])
+    comment = TextAreaField('Comment', validators=[DataRequired()])
+    submit = SubmitField('Post')
